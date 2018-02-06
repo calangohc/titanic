c8f2fb9d320a42af8301c0ea555e6f08b3f17a4f
Humberto Rocha <humrochagf@gmail.com>
2018-02-06 21:32:23 -0200

    make file run from any path

--- src/data/download_dataset.py
+++ src/data/download_dataset.py
@@ -54,19 +54,15 @@ def fetch_data_kaggle_titanic(path='data/raw/'):
 
 
 if __name__ == '__main__':
-    # TODO find a better way to find the absolute path for '../../data/raw'
-
     # Get current absolute path
-    absolute_path = os.getcwd()
+    absolute_path = os.path.abspath(__file__)
 
     # Get parent path
-    parent_path = os.path.split(absolute_path)[0]
-
-    # Get greatparent path
-    greatparent_path = os.path.split(parent_path)[0]
+    project_root = os.path.dirname(
+        os.path.dirname(os.path.dirname(absolute_path)))
 
     # Get final path (absolute path for '../../data/raw/'
-    final_path = os.path.join(greatparent_path, 'data/raw/')
+    final_path = os.path.join(project_root, 'data/raw/')
 
     # Fetch Titanic Data
     fetch_data_kaggle_titanic(final_path)
